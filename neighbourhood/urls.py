181226c734e69690dde160c791ee287e5ba7eea6
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-26 15:04:25 +0300

    Add a  pilow

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home, name = 'home'),
+    url(r'^tinymce/', include('tinymce.urls')),
     url(r'^search/', views.search_results, name='search'),
 
 
