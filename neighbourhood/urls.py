fc32303d578b33abe4dea039f46304eb92488a1c
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-26 15:23:12 +0300

    Add a  url for new message

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.home, name = 'home'),  
     url(r'^search/', views.search_results, name='search'),
-
+    url(r'^new/message$', views.new_message, name='new_message'),
 
 
  ]
