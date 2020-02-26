83ae0cd2dc444160b80e29ed977ef0d860de3335
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-26 15:18:26 +0300

    Add changes

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -4,13 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.home, name = 'home'),
-    url(r'^tinymce/', include('tinymce.urls')),
+    url('^$',views.home, name = 'home'),  
     url(r'^search/', views.search_results, name='search'),
 
 
 
  ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
  
